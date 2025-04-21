8f77555b8b6a59535c9525dbfdca44e9473e7901
Ægir <0x5ea000000@gmail.com>
2025-04-21 15:25:41 +0700

    temp2

--- cli/tests/test.rs
+++ cli/tests/test.rs
@@ -1,8 +1,15 @@
 #![cfg(test)]
 
 use borsh::{BorshDeserialize, BorshSerialize};
-use cli::solana::transaction::create_deposit_instructions_data;
-use darksol::{derive_pda, entrypoint::process_instruction};
+use cli::{
+    solana::transaction::{create_deposit_instructions_data, create_transfer_instructions_data},
+    utils::{generate_random_bytes, get_proof_from_file, read_json_file},
+};
+use darksol::{
+    derive_pda, entrypoint::process_instruction,
+    utils::account::get_associated_token_address_and_bump_seed,
+};
+use litesvm::LiteSVM;
 use solana_program::system_program::ID as SYSTEM_PROGRAM_ID;
 use solana_program_test::{ProgramTest, processor};
 use solana_sdk::{
@@ -15,8 +22,10 @@ use solana_sdk::{
 };
 use spl_associated_token_account::{
     get_associated_token_address, instruction::create_associated_token_account_idempotent,
+    tools::account,
 };
 use spl_token::instruction::sync_native;
+use types::UTXO;
 
 #[tokio::test]
 async fn test() {
@@ -27,12 +36,23 @@ async fn test() {
             .start()
             .await;
 
+    // let mut svm = LiteSVM::new();
+    // svm.airdrop(&from, 10_000).unwrap();
+    // svm.add_builtin(program_id);
+
     let depositor_keypair = solana_sdk::signature::Keypair::new();
-    let deposit_key = solana_sdk::signature::Keypair::new();
-    let view_key = solana_sdk::signature::Keypair::new();
-    let spend_key = solana_sdk::signature::Keypair::new();
+    let depositor_deposit_key = solana_sdk::signature::Keypair::new();
+    let depositor_view_key = solana_sdk::signature::Keypair::new();
+    let depositor_spend_key = solana_sdk::signature::Keypair::new();
 
     let depositor_pubkey = depositor_keypair.pubkey();
+
+    let receiver_keypair = solana_sdk::signature::Keypair::new();
+    let receiver_deposit_key = solana_sdk::signature::Keypair::new();
+    let receiver_view_key = solana_sdk::signature::Keypair::new();
+    let receiver_spend_key = solana_sdk::signature::Keypair::new();
+
+    let receiver_pubkey = receiver_keypair.pubkey();
 
     // Create and fund the depositor account
     // let depositor_account = solana_sdk::account::Account {
@@ -128,15 +148,14 @@ async fn test() {
 
     println!("Funding balance: {}", funding_balance);
 
-
     // deposit
 
     let mut deposit_data = match create_deposit_instructions_data(
         &spl_token::native_mint::ID,
         1_000_000_000,
-        spend_key.pubkey().to_bytes().to_vec(),
-        view_key.pubkey().to_bytes().to_vec(),
-        deposit_key.pubkey().to_bytes().to_vec(),
+        depositor_spend_key.pubkey().to_bytes().to_vec(),
+        depositor_view_key.pubkey().to_bytes().to_vec(),
+        depositor_deposit_key.pubkey().to_bytes().to_vec(),
         "test deposit".to_string(),
     ) {
         Ok(data) => data,
@@ -167,22 +186,24 @@ async fn test() {
     // system_program
     let mut account_metas: Vec<AccountMeta> = vec![];
 
-    let (funding_pda, _bump_seed) = Pubkey::find_program_address(&[b"funding_pda"], &program_id);
+    let (funding_pda, bump_seed) = Pubkey::find_program_address(&[b"funding_pda"], &program_id);
     account_metas.push(AccountMeta::new(funding_pda, false));
 
     account_metas.push(AccountMeta::new(depositor_pubkey, true));
 
     account_metas.push(AccountMeta::new(ata, false));
 
-    let (funding_ata_addr, _bump_seed) = Pubkey::find_program_address(
-        &[b"funding_ata", 
-        b"a"
-        // &spl_token::native_mint::ID.to_bytes()
-        ],
-        &program_id,
+    let (funding_ata, ata_bump) = get_associated_token_address_and_bump_seed(
+        &funding_pda,
+        &spl_token::native_mint::ID,
+        &spl_associated_token_account::ID,
+        &spl_token::ID,
     );
-    // let funding_ata_addr = get_associated_token_address(&funding_pda, &spl_token::native_mint::ID);
-    account_metas.push(AccountMeta::new(funding_ata_addr, false));
+
+    let (funding_ata, ata_bump) = Pubkey::find_program_address(&[b"funding_ata"], &program_id);
+
+    println!("bump {:?}", ata_bump);
+    account_metas.push(AccountMeta::new(funding_ata, false));
 
     account_metas.push(AccountMeta::new_readonly(spl_token::native_mint::ID, false));
 
@@ -199,6 +220,11 @@ async fn test() {
 
     account_metas.push(AccountMeta::new_readonly(
         solana_program::rent::sysvar::ID,
+        false,
+    ));
+
+    account_metas.push(AccountMeta::new_readonly(
+        spl_associated_token_account::ID,
         false,
     ));
 
@@ -228,6 +254,94 @@ async fn test() {
     }
 
     // transfer
+
+    // let utxo_in = UTXO::new(
+    //     depositor_spend_key.secret().to_bytes().to_vec(),
+    //     depositor_view_key.secret().to_bytes().to_vec(),
+    //     spl_token::native_mint::ID.to_bytes().to_vec(),
+    //     generate_random_bytes(32),
+    //     generate_random_bytes(12),
+    //     amount,
+    //     "a".to_string(),
+    // );
+
+    // let utxo_out = UTXO::new(
+    //     receiver_spend_key.secret().to_bytes().to_vec(),
+    //     receiver_view_key.secret().to_bytes().to_vec(),
+    //     spl_token::native_mint::ID.to_bytes().to_vec(),
+    //     generate_random_bytes(32),
+    //     generate_random_bytes(12),
+    //     amount,
+    //     "b".to_string(),
+    // );
+
+    // let inputs = vec![utxo_in];
+    // let outputs = vec![utxo_out];
+
+    // let mut serialized_data = match create_transfer_instructions_data(
+    //     &spl_token::native_mint::ID,
+    //     receiver_view_key.pubkey().to_bytes().to_vec(),
+    //     vec![],
+    //     inputs,
+    //     outputs,
+    //     vec![],
+    //     tree_number,
+    //     depositor_spend_key.to_bytes().to_vec(),
+    //     depositor_view_key.to_bytes().to_vec(),
+    // ) {
+    //     Ok(data) => data,
+    //     Err(err) => {
+    //         println!(
+    //             "{}",
+    //             format!("failed to create instruction data: {}", err.to_string())
+    //         );
+
+    //         return;
+    //     }
+    // };
+
+    // // get current tree number to fetch the correct commitments account info
+    // let newest_tree_number = 1;
+
+    // // get all necessary account meta
+    // // user wallet
+    // // spent commitments account
+    // // current commitments account
+    // // commitments manager account
+
+    // let account_metas: Vec<AccountMeta> = vec![];
+    // account_metas.push(AccountMeta::new(depositor_pubkey.clone(), true));
+
+    // let (spent_commitments_pda, _bump_seed) = derive_pda(tree_number, &program_id);
+    // account_metas.push(AccountMeta::new(spent_commitments_pda, false));
+
+    // let (current_commitments_pda, _bump_seed) = derive_pda(newest_tree_number, &program_id);
+    // account_metas.push(AccountMeta::new(current_commitments_pda, false));
+
+    // let (commitments_manager_pda, _bump_seed) =
+    //     Pubkey::find_program_address(&[b"commitments_manager_pda"], &program_id);
+    // account_metas.push(AccountMeta::new(commitments_manager_pda, false));
+
+    // // insert variant bytes
+    // serialized_data.insert(0, 1);
+    // // Create instruction
+    // let instruction = Instruction {
+    //     program_id,
+    //     accounts: account_metas,
+    //     data: serialized_data,
+    // };
+
+    // let message = Message::new(&[instruction], Some(&&depositor_pubkey));
+    // let mut transaction = Transaction::new_unsigned(message);
+
+    // transaction.sign(&[&depositor_keypair], recent_blockhash);
+
+    // let res = banks_client.process_transaction(transaction).await;
+
+    // match res {
+    //     Ok(_) => println!("Deposit transaction successful"),
+    //     Err(err) => println!("Deposit transaction failed: {:?}", err),
+    // }
 
     // withdraw
 }
